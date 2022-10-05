391510ec07318be6f8ad0803fd528d62e5d33a72
thisIsGloriaWu <gloriawu0924@gmail.com>
2022-10-04 17:22:03 -0700

    [A1] Features: implement data generator

---
+++ 6650_a1_jiayuewu/src/main/java/common/DataGenerator.java
@@ -0,0 +1,47 @@
+package common;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class DataGenerator {
+    private static final DataGenerator INSTANCE = new DataGenerator();
+
+    private final int MIN = 1;
+    private final int SEASON = 2022;
+    private final int SKIER_MAX = 100000;
+    private final int RESORT_MAX = 10;
+    private final int LIFT_MAX = 40;
+    private final int TIME_MAX = 360;
+
+    public static DataGenerator getInstance() {
+        return INSTANCE;
+
+    }
+    protected DataGenerator() {
+    }
+
+    public int getRandomSkierId() {
+        return ThreadLocalRandom.current().nextInt(MIN, SKIER_MAX+1);
+    }
+
+    public int getRandomResortId() {
+        return ThreadLocalRandom.current().nextInt(MIN, RESORT_MAX+1);
+    }
+
+    public int getRandomLiftId() {
+        return ThreadLocalRandom.current().nextInt(MIN, LIFT_MAX+1);
+    }
+
+    public int getRandomSeasonId() {
+        return SEASON;
+    }
+
+    public int getRandomDayId() {
+        return MIN;
+    }
+
+    public int getRandomTime() {
+        return ThreadLocalRandom.current().nextInt(MIN, TIME_MAX+1);
+    }
+
+}
