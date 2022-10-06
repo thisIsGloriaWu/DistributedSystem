f25f060b13489d210e9523ba28df0e71e32437ba
thisIsGloriaWu <gloriawu0924@gmail.com>
2022-10-05 20:39:15 -0700

    [A1] Tests: test with ResortsApiExample

--- 6650_a1_jiayuewu/src/main/java/example/ResortsApiExample.java
+++ 6650_a1_jiayuewu/src/main/java/example/ResortsApiExample.java
@@ -1,6 +1,7 @@
 package example;
 
 import io.swagger.client.*;
+import io.swagger.client.api.SkiersApi;
 import io.swagger.client.auth.*;
 import io.swagger.client.model.*;
 import io.swagger.client.api.ResortsApi;
@@ -12,14 +13,14 @@ public class ResortsApiExample {
 
     public static void main(String[] args) {
 
-        ResortsApi apiInstance = new ResortsApi();
-        apiInstance.getApiClient().setBasePath("http://35.92.182.244:8080");
+        SkiersApi apiInstance = new SkiersApi();
+        apiInstance.getApiClient().setBasePath("http://35.91.140.147:8080/swagger-spring");
         Integer resortID = 56; // Integer | ID of the resort of interest
         Integer seasonID = 56; // Integer | ID of the resort of interest
         Integer dayID = 56; // Integer | ID of the resort of interest
         try {
-            ResortSkiers result = apiInstance.getResortSkiersDay(resortID, seasonID, dayID);
-            System.out.println(result);
+            ApiResponse<Void> response = apiInstance.writeNewLiftRideWithHttpInfo(new LiftRide(), 1, "2022", "1", 1);
+            System.out.println(response);
         } catch (ApiException e) {
             System.err.println("Exception when calling ResortsApi#getResortSkiersDay");
             e.printStackTrace();
